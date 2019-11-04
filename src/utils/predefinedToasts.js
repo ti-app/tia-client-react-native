630e4c777364c3671976f78b023659f485ab777a
Akshay M <akshay.milmile@gmail.com>
2019-11-04 18:09:05 +0100

    Removed npx jetify from circleci

---
+++ src/utils/predefinedToasts.js
@@ -0,0 +1,66 @@
+import { Toast } from 'native-base';
+import * as colors from '../styles/colors';
+
+export const showWelcomeLoginToast = () => {
+	Toast.show({
+		text: 'Welcome! Successfully logged in',
+		buttonText: 'Okay',
+		style: { backgroundColor: colors.green },
+	});
+};
+
+export const showLoginFailed = () => {
+	Toast.show({
+		text: 'Login failed! Please heck your credentials.',
+		buttonText: 'Okay',
+		style: { backgroundColor: colors.red },
+	});
+};
+
+export const showNeedApproval = () => {
+	Toast.show({
+		text: 'Awaiting approval from moderator.',
+		buttonText: 'Okay',
+		style: { backgroundColor: colors.red },
+	});
+};
+
+export const showEmailSuccessfullToast = () => {
+	Toast.show({
+		text: 'Email has been sent! ',
+		buttonText: 'Okay',
+		style: { backgroundColor: colors.green },
+	});
+};
+
+export const showPasswordResetIssueToast = () => {
+	Toast.show({
+		text: 'Issue while reseting password',
+		buttonText: 'Okay',
+		style: { backgroundColor: colors.red },
+	});
+};
+
+export const showSuccessfulRegisterToast = () => {
+	Toast.show({
+		text: 'Welcome! Successfully registerd in TIA',
+		buttonText: 'Great',
+		style: { backgroundColor: colors.green },
+	});
+};
+
+export const showSomethingBadToast = () => {
+	Toast.show({
+		text: 'Something bad happened!',
+		buttonText: 'Oops',
+		style: { backgroundColor: colors.red },
+	});
+};
+
+export const showErrorToast = (errorMessage) => {
+	Toast.show({
+		text: '${errorMessage}',
+		buttonText: 'Oops',
+		style: { backgroundColor: colors.red },
+	});
+};
