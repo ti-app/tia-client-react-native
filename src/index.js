0623d6c8c22ad8ba140e4e660ef52f9098980c3a
Akshay M <akshay.milmile@gmail.com>
2019-11-12 18:15:38 +0100

    Fixed app crashing on drawer open for production build again

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import React, { useState, useEffect, useCallback } from 'react';
 import { Platform, StatusBar, StyleSheet, View, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
